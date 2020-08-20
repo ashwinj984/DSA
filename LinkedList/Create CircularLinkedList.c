c1f529ba781657f9fb4ded7e2884effb75c56f6d
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-08-20 16:16:59 +0530

    Update and rename CircularLinkedList.c to Create CircularLinkedList.c

--- LinkedList/Create CircularLinkedList.c
+++ LinkedList/Create CircularLinkedList.c
@@ -65,3 +65,59 @@ int main(){
     Display();
     return 0;
 }
+
+// next method using just tail pointer no use of head pointer
+
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+} *tail;
+typedef struct node Node;
+void create()
+{
+   tail = 0;
+    Node *newnode;
+    int choice = 1;
+    while (choice != 0)
+    {
+        newnode = (Node *)malloc(sizeof(Node));
+        printf("Enter data for Doubly Linked List: ");
+        scanf("%d", &newnode->data);
+        newnode->next = 0;
+        if(tail == 0){
+            tail = newnode;
+            tail->next = newnode;
+        }
+        else {
+            newnode->next = tail->next;
+            tail->next = newnode;
+            tail = newnode;
+        }
+        printf("Do you want to enter more element: ");
+        scanf("%d",&choice);
+    }
+}
+void Display(){
+    Node *temp;
+    if(tail == 0){
+        printf("NULL\n");
+    }
+    else{
+        temp = tail->next;
+        while(temp->next != tail->next){
+            printf("%d==>>",temp->data);
+            temp = temp->next;
+        }
+        printf("%d==>>",temp->data);
+    }
+    printf("NULL");
+}
+
+int main(){
+    create();
+    Display();
+}

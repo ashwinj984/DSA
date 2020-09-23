06aba305d3e73ddf6a56314861f33bfa4a871e45
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-09-23 18:44:57 +0530

    Create Merge Two sorted Linked List.java

---
+++ LinkedList/Merge Two sorted Linked List.java
@@ -0,0 +1,29 @@
+public static LinkedList mergeTwoSortedLists(LinkedList l1, LinkedList l2) {
+      LinkedList ml = new LinkedList();
+
+      Node one = l1.head;
+      Node two = l2.head;
+      while (one != null && two != null) {
+        if (one.data < two.data) {
+          ml.addLast(one.data);
+          one = one.next;
+        } else {
+          ml.addLast(two.data);
+          two = two.next;
+        }
+      }
+
+      while (one != null) {
+        ml.addLast(one.data);
+        one = one.next;
+      }
+
+      while (two != null) {
+        ml.addLast(two.data);
+        two = two.next;
+      }
+
+      return ml;
+    }
+    //It is the main logic not the whole code
+    

6f33343bc29ab713cf16502ea129a0f965ebc0c6
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-09-28 00:01:51 +0530

    Create Display of a Generic Tree.java

---
+++ Generic Tree/Display of a Generic Tree.java
@@ -0,0 +1,17 @@
+private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }

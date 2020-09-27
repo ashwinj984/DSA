8e5d16d23e987d2bb90b8fa142b407343ba532ce
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-09-28 00:01:01 +0530

    Create Constructor.java

---
+++ Generic Tree/Constructor.java
@@ -0,0 +1,23 @@
+ public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }

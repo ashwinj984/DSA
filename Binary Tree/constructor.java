3eaff508c1f70738a380241b99ad652a5f3952ef
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-10-14 18:53:18 +0530

    Create constructor.java

---
+++ Binary Tree/constructor.java
@@ -0,0 +1,62 @@
+public class constructor {
+	public static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int data, Node left, Node right) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static class Pair {
+		Node node;
+		int state;
+
+		Pair(Node node, int state) {
+			this.node = node;
+			this.state = state;
+		}
+	}
+  
+  public static Node construct(Integer[] arr) {
+		Node root = new Node(arr[0], null, null);
+		Pair rtp = new Pair(root, 1);
+
+		Stack<Pair> st = new Stack<>();
+		st.push(rtp);
+
+		int idx = 0;
+		while (st.size() > 0) {
+			Pair top = st.peek();
+			if (top.state == 1) {
+				idx++;
+				if (arr[idx] != null) {
+					top.node.left = new Node(arr[idx], null, null);
+					Pair lp = new Pair(top.node.left, 1);
+					st.push(lp);
+				} else {
+					top.node.left = null;
+				}
+
+				top.state++;
+			} else if (top.state == 2) {
+				idx++;
+				if (arr[idx] != null) {
+					top.node.right = new Node(arr[idx], null, null);
+					Pair rp = new Pair(top.node.right, 1);
+					st.push(rp);
+				} else {
+					top.node.right = null;
+				}
+
+				top.state++;
+			} else {
+				st.pop();
+			}
+		}
+
+		return root;
+	}

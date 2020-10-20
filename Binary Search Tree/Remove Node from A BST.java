aa103b7d35b2380ef3180a9008a7ee5699f47889
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-10-20 12:47:06 +0530

    Create Remove Node from A BST.java

---
+++ Binary Search Tree/Remove Node from A BST.java
@@ -0,0 +1,23 @@
+
+	  public static Node remove(Node node, int data) {
+	   if(data > node.data) {
+		   node.right = remove(node.right,data);
+	   }else if(data < node.data) {
+		   node.left = remove(node.left,data);
+	   }else {
+		   //Main Work
+		   if(node.left != null && node.right != null) {
+			   int lmax = max(node.left);
+			   node.data = lmax;
+			   node.left = remove(node.left,lmax);
+			   return node;
+		   }else if(node.left !=null) {
+			   return node.left;
+		   }else if(node.right!=null) {
+			   return node.right;
+		   }else {
+			   return null;
+		   }
+	   }
+	   return node;
+	  }

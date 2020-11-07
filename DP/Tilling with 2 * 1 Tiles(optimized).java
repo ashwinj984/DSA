5676ea4a563a10b907d919e90606e86b9cd4db1d
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-11-07 16:40:02 +0530

    100% works Tabulation

---
+++ DP/Tilling with 2 * 1 Tiles(optimized).java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int  n = scn.nextInt();
+        
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i < dp.length; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+    
+    
+}

e603b1a9f5439639a15200514fbbc10b06813f9a
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-11-13 20:57:49 +0530

    Create Buy and Sell Stock - infinite transaction Allowed.java

---
+++ DP/Buy and Sell Stock - infinite transaction Allowed.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        int bd = 0;
+        int sd = 0;
+        int profit = 0;
+        
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] >= arr[i - 1]){
+                sd++;
+            }else{
+                profit += arr[sd] - arr[bd];
+                sd = bd = i;
+            }
+        }
+        profit += arr[sd] - arr[bd];
+        System.out.println(profit);
+    }
+
+}

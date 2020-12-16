85c8208bb3cf7df276e5ebb73757ac02ebbb5814
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-12-16 19:04:37 +0530

    Create Count Of Substrings Having All Unique Characters.java

---
+++ HashMap and Heaps/Count Of Substrings Having All Unique Characters.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Main {
+
+	public static int solution(String str) {
+	    
+	    		int len = 0;
+		
+		int i = -1;
+		int j = -1;
+		HashMap<Character, Integer> map = new HashMap<>();
+		while(true) {
+			
+			boolean f1 = false;
+			boolean f2 = false;
+			while(i < str.length() - 1) {
+				f1 = true;
+			
+			i++;
+			char ch = str.charAt(i);
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
+			if(map.get(ch) == 2) {
+				break;
+			}else {
+				len += i - j;
+			}
+			
+		
+		}
+		
+		while(j < i) {
+			f2 = true;
+			j++;
+			char ch = str.charAt(j);
+			map.put(ch, map.get(ch) - 1);
+			if(map.get(ch) == 1) {
+				len += i - j;
+				break;
+			}
+			
+		}
+		
+		if(f1 == false && f2 == false) {
+			break;
+		}
+	}
+		return len;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+
+}

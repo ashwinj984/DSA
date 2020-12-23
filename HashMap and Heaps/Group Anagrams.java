6620557da9fbc0d3ac4b882acbe13b1b5b87f609
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-12-23 21:17:37 +0530

    Create Group Anagrams.java

---
+++ HashMap and Heaps/Group Anagrams.java
@@ -0,0 +1,71 @@
+import java.util.*;
+    
+    public class Main {
+    
+        public static ArrayList < ArrayList < String >> groupAnagrams(String[] strs) {
+            HashMap<HashMap<Character,Integer>,ArrayList<String>> bmap = new HashMap<>();
+            for (String str: strs) {
+                HashMap < Character, Integer > map = new HashMap < > ();
+                for (int i = 0; i < str.length(); i++) {
+                    char ch = str.charAt(i);
+                    map.put(ch, map.getOrDefault(ch, 0) + 1);
+                }
+    
+                if (bmap.containsKey(map) == false) {
+                    ArrayList<String> arr = new ArrayList<>();
+                    arr.add(str);
+                    bmap.put(map,arr);
+                }else{
+                    ArrayList<String> list  = bmap.get(map);
+                    list.add(str);
+                }
+                
+            }
+            ArrayList<ArrayList<String>> res = new ArrayList<>();
+                for(ArrayList<String> val  : bmap.values() ){
+                    res.add(val);
+                }
+                return res;
+        }
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            String[] arr = new String[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.next();
+            }
+            ArrayList < ArrayList < String >> anagramsGrouped = groupAnagrams(arr);
+            for (ArrayList < String > lst: anagramsGrouped) {
+                Collections.sort(lst);
+            }
+            anagramsGrouped.sort(new ListComparator());
+            display(anagramsGrouped);
+        }
+    
+        // it is used to make the result unique
+        static class ListComparator implements Comparator < List < String >> {
+            @Override
+            public int compare(List < String > l1, List < String > l2) {
+                if (l1.size() != l2.size()) {
+                    return l2.size() - l1.size();
+                }
+    
+                String l1str = l1.get(0);
+                String l2str = l2.get(0);
+                return l1str.compareTo(l2str);
+    
+            }
+        }
+    
+        public static void display(ArrayList < ArrayList < String >> list) {
+            for (int i = 0; i < list.size(); i++) {
+                ArrayList < String > currList = list.get(i);
+                for (int j = 0; j < currList.size(); j++) {
+                    System.out.print(currList.get(j) + " ");
+                }
+                System.out.println();
+            }
+        }
+    
+    }

dfa2107878d5dfc0e7722ced13fa9583f88b8fb1
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-12-24 12:08:00 +0530

    Update Group Shifter Strings.java

--- HashMap and Heaps/Group Shifter Strings.java
+++ HashMap and Heaps/Group Shifter Strings.java
@@ -7,9 +7,9 @@ public class Main {
         String key = "";
         for (int i = 1; i < str.length(); i++) {
             char curr = str.charAt(i);
-            char prev = str.charAt(i);
+            char prev = str.charAt(i-1);
             int diff = curr - prev;
-            if (diff < 'a') {
+            if (diff < 0) {
                 diff += 26;
             }
 
@@ -21,20 +21,20 @@ public class Main {
     }
     public static ArrayList < ArrayList < String >> groupShiftedStrings(String[] strs) {
         HashMap < String, ArrayList < String > > map = new HashMap < > ();
-        for(String str : strs){
+        for (String str: strs) {
             String key = getKey(str);
-            if(map.containsKey(key) == false){
-                ArrayList<String> list = new ArrayList<>();
+            if (map.containsKey(key) == false) {
+                ArrayList < String > list = new ArrayList < > ();
                 list.add(str);
-                map.put(key,list);
-            }else{
-                ArrayList<String> list = map.get(key);
+                map.put(key, list);
+            } else {
+                ArrayList < String > list = map.get(key);
                 list.add(str);
             }
         }
-        
-        ArrayList<ArrayList<String>> res = new ArrayList<>();
-        for(ArrayList<String> list : map.values()){
+
+        ArrayList < ArrayList < String >> res = new ArrayList < > ();
+        for (ArrayList < String > list: map.values()) {
             res.add(list);
         }
         return res;

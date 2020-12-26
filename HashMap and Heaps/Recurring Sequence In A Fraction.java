5374105c56d8fdf8e8d71e4f606c28d8e61b4f70
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2020-12-26 16:38:04 +0530

    Create Recurring Sequence In A Fraction.java

---
+++ HashMap and Heaps/Recurring Sequence In A Fraction.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+
+    public static String solution(int num, int den) {
+        StringBuilder ans = new StringBuilder();
+
+        int q = num / den;
+        int r = num % den;
+        ans.append(q);
+        if (r == 0)
+            return ans.toString();
+        else {
+            ans.append(".");
+            HashMap < Integer, Integer > map = new HashMap < > ();
+
+            while (r != 0) {
+                if (map.containsKey(r)) {
+                    int pos = map.get(r);
+                    ans.insert(pos, "(");
+                    ans.append(")");
+                    break;
+                } else {
+                    map.put(r,ans.length());
+                    r *= 10;
+                    q = r / den;
+                    r = r % den;
+                    ans.append(q);
+                }
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        int den = scn.nextInt();
+        System.out.println(solution(num, den));
+    }
+
+}

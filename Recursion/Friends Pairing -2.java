cc67d79742c4d9591a60f8786e25ac67648daade
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2021-01-26 23:16:51 +0530

    Create Friends Pairing -2.java

---
+++ Recursion/Friends Pairing -2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class FriendPairing2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		boolean[] used = new boolean[n + 1];
+		solution(1, n, used, "");
+
+	}
+
+	static int counter = 1;
+
+	public static void solution(int i, int n, boolean[] used, String asf) {
+
+		if (i > n) {
+			System.out.println(counter + "." + asf);
+			counter++;
+			return;
+		}
+
+		if (used[i] == true) {
+			solution(i + 1, n, used, asf);
+		} else {
+			used[i] = true;
+			solution(i + 1, n, used, asf + "(" + i + ") ");
+			for (int j = i + 1; j <= n; j++) {
+				if (used[j] == false) {
+					used[j] = true;
+					solution(i + 1, n, used, asf + "(" + i + "," + j + ") ");
+					used[j] = false;
+				}
+			}
+			used[i] = false;
+		}
+	}
+
+}

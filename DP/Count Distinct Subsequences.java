d2c2939b9e326b6bb2c70b7c3b962fa3de00b307
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2021-02-04 17:31:37 +0530

    Update Count Distinct Subsequences.java

--- DP/Count Distinct Subsequences.java
+++ DP/Count Distinct Subsequences.java
@@ -1,25 +1,26 @@
 import java.util.*;
+public class Count_Distinct_Subsequences {
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        String str = scn.next();
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		
+		HashMap<Character,Integer> map = new HashMap<>();
+		long[] dp = new long[str.length() + 1];
+		
+		dp[0] = 1;
+		
+		for(int i = 1; i < dp.length; i++) {
+			dp[i] = 2 * dp[i - 1];
+			if(map.containsKey(str.charAt(i - 1))) {
+				int idx = map.get(str.charAt(i - 1));
+				dp[i] = dp[i] - dp[idx - 1];
+			}
+			map.put(str.charAt(i-1),i);
+			
+		}
+		System.out.println(dp[str.length()] - 1);
 
-        //write your code here
-        long[] dp = new long[str.length() + 1];
-        dp[0] = 1;
+	}
 
-        HashMap < Character, Integer > map = new HashMap < > ();
-        for (int i = 1; i < dp.length; i++) {
-            dp[i] = 2 * dp[i - 1];
-            char ch = str.charAt(i);
-            if (map.containsKey(ch)) {
-                int j = map.get(ch);
-                dp[i] = dp[i] - dp[j - 1];
-            }
-            map.put(ch, i);
-        }
-        System.out.println(dp[str.length()] - 1);
-
-    }
 }

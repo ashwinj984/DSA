6c4dff97cb882d80c39e92c5eb05045fcd6fadff
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2021-02-11 16:19:57 +0530

    Create Print All Results In 0-1 Knapsack.java

---
+++ DP/Print All Results In 0-1 Knapsack.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static class Pair{
+        int i;
+        int j;
+        String psf;
+        Pair(int i,int j,String psf){
+            this.i = i;
+            this.j = j;
+            this.psf = psf;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] values = new int[n];
+        String str1 = br.readLine();
+        for (int i = 0; i < n; i++) {
+            values[i] = Integer.parseInt(str1.split(" ")[i]);
+        }
+
+        int[] wts = new int[n];
+        String str2 = br.readLine();
+        for (int i = 0; i < n; i++) {
+            wts[i] = Integer.parseInt(str2.split(" ")[i]);
+        }
+
+        int cap = Integer.parseInt(br.readLine());
+
+        //write your code here
+        int[][] dp = new int[values.length + 1][cap + 1];
+        
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 1; j < dp[0].length; j++){
+                dp[i][j] = dp[i - 1][j];
+                
+                if(wts[i - 1] <= j){
+                    if(dp[i - 1][j - wts[i - 1]] + values[i - 1] > dp[i - 1][j]){
+                        dp[i][j] = dp[i - 1][j - wts[i - 1]] + values[i - 1];
+                    }
+                }
+            }
+        }
+        System.out.println(dp[values.length][cap]);
+        
+        ArrayDeque<Pair> q =  new ArrayDeque<>();
+        q.add(new Pair(n, cap, ""));
+        
+        while(q.size() > 0){
+            Pair rem = q.removeFirst();
+            
+            if(rem.i == 0 || rem. j == 0){
+                System.out.println(rem.psf);
+                continue;
+            }
+            
+            int exc = dp[rem.i - 1][rem.j];
+            if(rem.j >= wts[rem. i - 1]){
+                int inc = dp[rem.i - 1][rem.j - wts[rem. i - 1]] + values[rem.i - 1];
+                
+                if(dp[rem.i][rem.j] == inc){
+                    q.add(new Pair(rem.i - 1, rem.j - wts[rem. i - 1], (rem.i - 1) + " " +  rem.psf ));
+                }
+            }
+            
+            if(dp[rem.i][rem.j] == exc){
+                q.add(new Pair(rem.i - 1, rem.j, rem.psf));
+            }
+        }
+    }
+}

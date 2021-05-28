91e35b4a56ae369c550c21b5730c86bd1aa8b415
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2021-05-28 22:46:01 +0530

    Create Majority Element 1.java

---
+++ Arrays and Strings/Majority Element 1.java
@@ -0,0 +1,60 @@
+// O(n) time
+// O(1) space
+
+import java.util.*;
+
+public class Main {
+
+    // ~~~~~~~~~~~~~~~~~~~~~User Section~~~~~~~~~~~~~~~~~~~~~
+    
+    public static int validCandidate(int[] arr){
+        int val = arr[0];
+        int count = 1;
+        
+        for(int i = 1; i < arr.length; i++){
+            if(val == arr[i]){
+                count++;
+            }else{
+                count--;
+            }
+            
+            if(count == 0){
+                val = arr[i];
+                count = 1;
+            }
+        }
+        return val;
+    }
+    public static void printMajorityElement(int[] arr) {
+        // write your code here
+        int val = validCandidate(arr);
+        // val have potential candidate for majority element
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == val){
+                count++;
+            }
+        }
+        if(count > arr.length / 2){
+            System.out.println(val);
+        }else{
+            System.out.println("No Majority Element exist");
+        }
+        
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~Input Management~~~~~~~~~~~~~~~~~~~
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        printMajorityElement(arr);
+    }
+}

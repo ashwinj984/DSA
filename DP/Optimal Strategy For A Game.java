0518bf35dcdc6ce0f444248df4c603092dd3d341
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2022-01-17 12:50:52 +0530

    Update Optimal Strategy For A Game.java

--- DP/Optimal Strategy For A Game.java
+++ DP/Optimal Strategy For A Game.java
@@ -31,5 +31,37 @@ public class Main {
 		}
 		optimalStrategy(a);
 	}
+}
 
+// by memoization
+
+import java.util.*;
+public class OptimalStrategyForAGame {
+	public static void main(String[] args) {
+		int[] arr = {5,3,7,10};
+		int[][] dp = new int[arr.length][arr.length];
+		for(int i = 0; i < dp.length; i++) {
+			for(int j = 0; j < dp.length; j++) {
+				dp[i][j] = -1;
+			}
+		}
+		System.out.println(optimalValue(arr, 0, arr.length - 1, dp));
+	}
+	
+	public static int optimalValue(int[] arr, int i, int j, int[][] dp) {
+		if(i == j) {
+			return arr[i];
+		}
+		if(j == i + 1) {
+			return Math.max(arr[i], arr[j]);
+		}
+		if(dp[i][j] != -1) {
+			return dp[i][j];
+		}
+		
+		int val1 = arr[i] + Math.min(optimalValue(arr, i + 2, j, dp), optimalValue(arr, i + 1, j - 1, dp));
+		int val2 = arr[j] + Math.min(optimalValue(arr, i + 1, j - 1, dp), optimalValue(arr, i, j -2, dp));
+		dp[i][j] = Math.max(val1, val2); 
+		return dp[i][j];
+	}
 }

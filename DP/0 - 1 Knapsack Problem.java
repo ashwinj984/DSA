c13e3933c35deb1a9217da4782e9ff9d10974961
ashwinj984 <56488757+ashwinj984@users.noreply.github.com>
2022-03-30 17:55:07 +0530

    Update 0 - 1 Knapsack Problem.java

--- DP/0 - 1 Knapsack Problem.java
+++ DP/0 - 1 Knapsack Problem.java
@@ -26,3 +26,22 @@ class Solution
     }
 }
 // tabulation method
+
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int w, int wt[], int val[], int n) 
+    { 
+        int[][] t = new int[n + 1][w + 1];
+        for(int i = 1; i < n + 1; i++){
+            for(int j = 1; j < w + 1; j++){
+                if(wt[i - 1] <= j){
+                    t[i][j] = Math.max(val[i - 1]  + t[i - 1][j - wt[i - 1]], t[i - 1][j]);
+                }else{
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+        return t[n][w];
+    } 
+}
